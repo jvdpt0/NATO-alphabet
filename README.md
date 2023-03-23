caaa2a18639e3971833122b3334361ad73951e4e
jvdpt0 <jvdpt0@gmail.com>
2023-03-23 15:58:53 -0300

    Adding Readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # NATO-alphabet
- 
+Simple project using pandas, csv and dictionary comprehension to convert any word to NATO alphabet characters (Alfa, Bravo, Charlie...)
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-    
-{"A": "Alfa", "B": "Bravo"}
 data = pd.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row['letter']:row['code'] for (index,row) in data.iterrows()}
 
