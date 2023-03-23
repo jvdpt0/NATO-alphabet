fc038f708384e9419888e3bc34668d98bd8134f3
jvdpt0 <jvdpt0@gmail.com>
2023-03-23 16:12:31 -0300

    Added try except

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ import pandas as pd
 data = pd.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row['letter']:row['code'] for (index,row) in data.iterrows()}
 
-word = input('Enter a word: ').upper()
-nato_word = [nato_dict[letter] for letter in word]
-print (nato_word)
+success = False
+
+while success == False:
+
+    try:
+        word = input('Enter a word: ').upper()
+        nato_word = [nato_dict[letter] for letter in word]
+        print (nato_word)
+        success = True
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        
 
