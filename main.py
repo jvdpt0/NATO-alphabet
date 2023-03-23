2ba28d55e576658d6e8799ce5b5b5da51bfd8a5a
jvdpt0 <jvdpt0@gmail.com>
2023-03-23 15:56:49 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+    
+{"A": "Alfa", "B": "Bravo"}
+data = pd.read_csv('nato_phonetic_alphabet.csv')
+nato_dict = {row['letter']:row['code'] for (index,row) in data.iterrows()}
+
+word = input('Enter a word: ').upper()
+nato_word = [nato_dict[letter] for letter in word]
+print (nato_word)
+
